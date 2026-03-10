24f58e0eea5297b275c8d284b3c840d320dc86dd
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2026-03-11 00:09:44 +0500

    Update Footer component to display the current year dynamically in the copyright notice, enhancing accuracy and relevance.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="w-full flex justify-center items-center p-5 mt-auto">
-      <p className="text-sm text-white">Copyright © 2025 Jin Jessica Yang</p>
+      <p className="text-sm text-white">Copyright © {currentYear} Jin Jessica Yang</p>
     </div>
   );
 };
